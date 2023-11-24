de66c70a3397b6159fa86705ef790fda211d5293
Clementine <clementine.mory@gmail.com>
2023-11-24 10:24:37 +0100

    vercel deployment backend

--- db.js
+++ db.js
@@ -2,8 +2,20 @@ require('dotenv').config();
 
 const Pool = require('pg').Pool;
 
+// const dbUser = process.env.USER;
+// const dbHost = process.env.HOST;
+// const dbName = process.env.DATABASE;
+// const dbPass = process.env.PASSWORD;
+
+
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-})
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  })
+
+  pool.connect((err) => {
+    if (err) throw err
+    console.log('Connect to PostgreSQL successfully')
+  })
+
 
 module.exports = pool;
