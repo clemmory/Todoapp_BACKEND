89932bd19640c3d103f95c7172bbb6244fee046c
Clementine <clementine.mory@gmail.com>
2023-11-24 12:23:41 +0100

    vercel

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ var app = express();
 const cors = require('cors');
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://vercel.com/clemmory/todoapp-backend'); // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- db.js
+++ db.js
@@ -2,12 +2,6 @@ require('dotenv').config();
 
 const Pool = require('pg').Pool;
 
-// const dbUser = process.env.USER;
-// const dbHost = process.env.HOST;
-// const dbName = process.env.DATABASE;
-// const dbPass = process.env.PASSWORD;
-
-
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
   })
