7a467e0af731d6e620e249a3e813f7595c16e1fe
Clementine <clementine.mory@gmail.com>
2024-06-27 08:38:36 +0200

    MAJ backend express

--- db.js
+++ db.js
@@ -1,26 +1,29 @@
 require('dotenv').config();
 
-const Pool = require('pg').Pool;
+const { Pool } = require('pg');
 
-const dbUser = process.env.USER;
-const dbHost = process.env.HOST
-const dbName = process.env.DATABASE;
-const dbPass = process.env.PASSWORD;
-
+const dbUser = process.env.DB_USER;
+const dbHost = process.env.DB_HOST
+const dbName = process.env.DB_DATABASE;
+const dbPass = process.env.DB_PASSWORD;
+const dbPort = process.env.DB_PORT;
 
 const pool = new Pool({
     user: dbUser,
     host: dbHost,
     database: dbName,
     password: dbPass,
-    port: 5432,
+    port: dbPort,
+  });
 
-  })
-
-  pool.connect((err) => {
-    if (err) throw err
-    console.log('Connect to PostgreSQL successfully')
-  })
+// Test connection
+pool.query('SELECT 1', (err, res) => {
+  if (err) {
+    console.error('Error connecting to PostgreSQL', err.stack);
+  } else {
+    console.log('Connected to PostgreSQL successfully');
+  }
+});
 
 
 module.exports = pool;
